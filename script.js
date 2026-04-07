bc32c4e93e14ccf88a105019d5e32f0f9cd02797
kakakakafufufu <chuir616@gmail.com>
2026-04-07 20:53:52 +0000

    Play

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// add javascript here
+let answer = 0
+let GuessCount = 0
+let totalwin = 0
+let totalGuess = 0
+let scores = []
+let playname = prompt("input your name")
+
+//Select Level
+document.getElementById("playBtn").addEventListener("click", function(){
+    let radios = document.getElementsByName("level")
+    let range = 3
+    for (let i=0;i < radios.length; i++){
+        if(radios[i].checked){
+            range = parseInt(radios[i].value)
+        }
+    }
+    answer = Math.floor(Math.random()*range)+1;
+
+    document.getElementById("msg").textContent = playname + ", guess a number between 1 and "+ range
+    document.getElementById("guess").value = 0
+    document.getElementById("guessBtn").disabled = false
+    document.getElementById("giveUpBtn").disabled = false
+    document.getElementById("playBtn").disabled = true
+
+    let levelRadios = document.getElementsByName("level")
+    for (let i=0;i < levelRadios.length; i++){
+        levelRadios[i].disabled = true
+    }
+})
+
