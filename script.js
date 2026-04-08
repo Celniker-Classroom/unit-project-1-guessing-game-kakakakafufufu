fc270a20ba4009726c64012761a399d83e639bd1
kakakakafufufu <chuir616@gmail.com>
2026-04-08 21:12:21 +0000

    leader board

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let answer = 0
 let GuessCount = 0
 let totalwin = 0
 let totalGuess = 0
+let range = 3
 let scores = []
 let playname = prompt("input your name")
-
+//player name 
+playname = playname.charAt(0).toUpperCase() + playname.slice(1);
 //Select Level
 document.getElementById("playBtn").addEventListener("click", function(){
     let radios = document.getElementsByName("level")
-    let range = 3
     for (let i=0;i < radios.length; i++){
         if(radios[i].checked){
             range = parseInt(radios[i].value)
@@ -26,16 +27,21 @@ document.getElementById("playBtn").addEventListener("click", function(){
     for (let i=0;i < levelRadios.length; i++){
         levelRadios[i].disabled = true
     }
-
-    //guess part
-    document.getElementById("guessBtn").addEventListener("click", function guess(){
-    let guess = document.getElementById("guess").value
+})
+//guess part
+document.getElementById("guessBtn").addEventListener("click", function guess(){
+    let input = document.getElementById("guess").value
+    let guess = parseInt(input)
     let temp = ""
+    //if it's a number in range
     if (guess < 1 || guess > range || isNaN(guess)) {
     document.getElementById("msg").textContent = "Pls give a number between 1 and "+range;
     document.getElementById("guess").value = ""
     return;
     }
+    GuessCount++;
+
+    //judge part 
     while (guess != answer) {
         totalGuess++;
         if(Math.abs(guess - answer) <= 2){
@@ -51,24 +57,39 @@ document.getElementById("playBtn").addEventListener("click", function(){
         document.getElementById("msg").textContent = "low but it's "+temp;
         }
         return;
-    }
-    totalGuess++;
-    totalwin++;
-    document.getElementById("wins").textContent = "Total wins: "+ totalwin
-    document.getElementById("avgScore").textContent = "Average Score: "+ totalGuess/totalwin
-    document.getElementById("msg").textContent = "Correct,you win this game!";
+    }    
+    document.getElementById("msg").textContent = "Correct,you win this game in "+totalGuess+" tries.";
     document.getElementById("guessBtn").disabled = true
+
+    updateScore(totalGuess)
 })
-    //reset part
+
+//reset part
     document.getElementById("giveUpBtn").addEventListener("click", function reset(){
             document.getElementById("msg").textContent = "GG"
-        for (let i=0;i < levelRadios.length; i++){
-        levelRadios[i].disabled = false
-    }
-        document.getElementById("guess").value = 0
+        let radios = document.getElementsByName("level")
+        for (let i=0;i < radios.length; i++){
+        radios[i].disabled = false}
         document.getElementById("guessBtn").disabled = true
         document.getElementById("giveUpBtn").disabled = true
         document.getElementById("playBtn").disabled = false
 })
-})
 
+function updateScore(score){
+    totalGuess += score;
+    totalwin++;
+    document.getElementById("wins").textContent = "Total wins: "+ totalwin
+    document.getElementById("avgScore").textContent = "Average Score: "+ (totalGuess/totalwin).toFixed(1)
+    //leader board
+    scores.push(score)
+    scores.sort(function(a,b){return a-b})
+
+    let leaderboard = document.getElementsByName("leaderboard")
+    for (let i=0;i < leaderboard.length; i++){
+        if (i < scores.length){
+        leaderboard[i].textContent = scores[i]
+        }else{
+        leaderboard[i].textContent = "--"
+        }
+    }
+}
