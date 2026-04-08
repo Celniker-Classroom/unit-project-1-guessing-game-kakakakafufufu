93ad2c87054f2fc97a6720fb0b92bb43e2110585
kakakakafufufu <chuir616@gmail.com>
2026-04-08 20:12:57 +0000

    guess & reset

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById("playBtn").addEventListener("click", function(){
             range = parseInt(radios[i].value)
         }
     }
-    answer = Math.floor(Math.random()*range)+1;
+    answer = Math.floor(Math.random() * range)+1;
 
     document.getElementById("msg").textContent = playname + ", guess a number between 1 and "+ range
     document.getElementById("guess").value = 0
@@ -26,5 +26,49 @@ document.getElementById("playBtn").addEventListener("click", function(){
     for (let i=0;i < levelRadios.length; i++){
         levelRadios[i].disabled = true
     }
+
+    //guess part
+    document.getElementById("guessBtn").addEventListener("click", function guess(){
+    let guess = document.getElementById("guess").value
+    let temp = ""
+    if (guess < 1 || guess > range || isNaN(guess)) {
+    document.getElementById("msg").textContent = "Pls give a number between 1 and "+range;
+    document.getElementById("guess").value = ""
+    return;
+    }
+    while (guess != answer) {
+        totalGuess++;
+        if(Math.abs(guess - answer) <= 2){
+            temp = "hot"
+        }else if(Math.abs(guess - answer) <= 5){
+            temp = "warm"
+        }else{
+            temp = "cold"
+        }
+        if (guess > answer) {
+        document.getElementById("msg").textContent = "high but it's "+temp;
+        } else {
+        document.getElementById("msg").textContent = "low but it's "+temp;
+        }
+        return;
+    }
+    totalGuess++;
+    totalwin++;
+    document.getElementById("wins").textContent = "Total wins: "+ totalwin
+    document.getElementById("avgScore").textContent = "Average Score: "+ totalGuess/totalwin
+    document.getElementById("msg").textContent = "Correct,you win this game!";
+    document.getElementById("guessBtn").disabled = true
+})
+    //reset part
+    document.getElementById("giveUpBtn").addEventListener("click", function reset(){
+            document.getElementById("msg").textContent = "GG"
+        for (let i=0;i < levelRadios.length; i++){
+        levelRadios[i].disabled = false
+    }
+        document.getElementById("guess").value = 0
+        document.getElementById("guessBtn").disabled = true
+        document.getElementById("giveUpBtn").disabled = true
+        document.getElementById("playBtn").disabled = false
+})
 })
 
